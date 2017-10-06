60135a907e157206859f6e9bf59fbaac3bbf5d58
lijian <fkhacker@vip.qq.com>
2017-10-07 00:01:01 +0800

    修正源

--- multi-node/generic/controller-install.sh
+++ multi-node/generic/controller-install.sh
@@ -92,6 +92,21 @@ function init_flannel {
 }
 
 function init_templates {
+    docker pull registry.cn-hangzhou.aliyuncs.com/google-containers/pause-amd64:3.0
+    docker tag registry.cn-hangzhou.aliyuncs.com/google-containers/pause-amd64:3.0 gcr.io/google_containers/pause-amd64:3.0
+
+    docker pull registry.cn-hangzhou.aliyuncs.com/google-containers/kubedns-amd64:1.9
+    docker tag registry.cn-hangzhou.aliyuncs.com/google-containers/kubedns-amd64:1.9 gcr.io/google_containers/kubedns-amd64:1.9
+
+    docker pull registry.cn-hangzhou.aliyuncs.com/google-containers/kube-dnsmasq-amd64:1.4
+    docker tag registry.cn-hangzhou.aliyuncs.com/google-containers/kube-dnsmasq-amd64:1.4 gcr.io/google_containers/kube-dnsmasq-amd64:1.4
+
+    docker pull registry.cn-hangzhou.aliyuncs.com/google-containers/exechealthz-amd64:1.2
+    docker tag registry.cn-hangzhou.aliyuncs.com/google-containers/exechealthz-amd64:1.2 gcr.io/google_containers/exechealthz-amd64:1.2
+
+    docker pull registry.cn-hangzhou.aliyuncs.com/google-containers/kubernetes-dashboard-amd64:v1.5.0
+    docker tag registry.cn-hangzhou.aliyuncs.com/google-containers/kubernetes-dashboard-amd64:v1.5.0 gcr.io/google_containers/kubernetes-dashboard-amd64:v1.5.1
+
     local TEMPLATE=/etc/systemd/system/kubelet.service
     local uuid_file="/var/run/kubelet-pod.uuid"
     if [ ! -f $TEMPLATE ]; then
